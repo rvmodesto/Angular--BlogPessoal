d4c60418349bc33f260a0a11714423baeeb00a10
Raquel Modesto <raquel.modesto2@gmail.com>
2021-09-29 23:26:22 -0300

    Criando rotas de entrar e cadastrar

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+export class UseLoginr{
+    public id: number;
+    public nome: string;
+    public usuario: string;
+    public senha: string;
+    public token: string;
+    public dataNascimento: Date;
+    public tipoUsuario: string;
+    public foto: string;
+}
