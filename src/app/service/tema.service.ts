4bf4247a1a7acd2fcbf6b34a11073beb9ae2379e
Raquel Modesto <raquel.modesto2@gmail.com>
2021-10-10 13:06:27 -0300

    Implementação do get e post de temas

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTemas(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://berakhot.herokuapp.com/tema', this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://berakhot.herokuapp.com/tema', this.token)
+  }
+
+}
--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,9 +11,12 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
   
+  tema: Tema = new Tema()
+  listaTemas: Tema[] 
 
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService 
   ) { }
 
   ngOnInit() {
@@ -19,6 +24,23 @@ export class TemaComponent implements OnInit {
       alert('Sua seção expirou, faça o login novamente.')
       this.router.navigate(['/entrar'])
     }
+
+    this.getAllTemas()
   }
 
+  getAllTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.getAllTemas()
+      this.tema = new Tema()
+    })
+  }
+  
 }
