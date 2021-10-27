4e2b1bbaf3c15861c852c9fbf94de631e31c156d
rvmodesto <raquel.modesto2@gmail.com>
2021-10-27 17:08:30 -0300

    Implementação de put-delete de postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -27,36 +27,23 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://berakhot.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://berakhot.herokuapp.com/postagens/${id}`, this.token)
+
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://berakhot.herokuapp.com/postagens', postagem, this.token)
   }
 
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://berakhot.herokuapp.com/postagens', postagem, this.token    )
+  }
+
+ //Utiliza-se Observable para quando temos que retornar alguma coisa, por isso delete não se utiliza
+  deletePostagem(id: number){
+    return this.http.delete(`https://berakhot.herokuapp.com/postagens/${id}`, this.token)
+
+  }
+
 }
-
-
-// import { Observable } from 'rxjs';
-// import { environment } from './../../environments/environment.prod';
-// import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { Postagem } from '../model/Postagem';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class PostagemService {
-
-//   constructor(private http: HttpClient) { }
-
-//   token = {
-//     headers: new HttpHeaders().set('Authorization', environment.token)
-//   }
-
-//   getAllPostagens(): Observable<Postagem[]>{
-//     return this.http.get<Postagem[]>('https://berakhot.herokuapp.com/postagens', this.token)
-//   }
-
-//   postPostagem(postagem: Postagem) : Observable<Postagem>{
-//     return this.http.post<Postagem>('https://berakhot.herokuapp.com/postagens', postagem, this.token)
-//   }
-
-// }
